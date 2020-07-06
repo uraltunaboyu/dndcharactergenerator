bf1fde6a317da2ee615b57f3487031d2e527e3c6
uraltunaboyu <ural2000@gmail.com>
2020-07-05 18:53:25 -0700

    Changed how character stats are handled

--- src/model/Character.java
+++ src/model/Character.java
@@ -8,18 +8,32 @@ import java.util.*;
 public class Character {
     public Class characterClass;
     public Race characterRace;
-    private enum statName {STR, DEX, CON, INT, WIS, CHA};
-    private enum
-    public LinkedHashMap<statName, Integer> stats = new LinkedHashMap<>();
+    public static final int STR = 0;
+    public static final int DEX = 1;
+    public static final int CON = 2;
+    public static final int INT = 3;
+    public static final int WIS = 4;
+    public static final int CHA = 5;
+    public int[] stats = new int[6];
+    private int characterLevel;
+    private int proficiencyBonus;
 
-    public Character(boolean normalRoll) {
-        for (statName stat: statName.values()) {
-            stats.put(stat, normalRoll ? generateStatNormal() : generateStatAdv());
-        }
+    public int getCharacterLevel() {
+        return characterLevel;
     }
 
-    public LinkedHashMap<statName, Integer> getStats() {
-        return stats;
+    public int getProficiencyBonus() {
+        return proficiencyBonus;
+    }
+
+    public Character(boolean normalRoll, int characterLevel) {
+        this.characterLevel = characterLevel;
+        for (int i = 0; i < 6; i++) {
+            stats[i] = (normalRoll ? generateStatNormal() : generateStatAdv());
+        }
+        //Proficiency bonus = (level/4) + 1, rounded up
+        proficiencyBonus = (characterLevel / 4) + 1 + (characterLevel % 4 == 0 ? 0 : 1);
+        //^ This is a really hacky way of simulating that. It sucks and I hate it.
     }
 
     private static int generateStatNormal() {
--- src/ui/CharacterCreator.java
+++ src/ui/CharacterCreator.java
@@ -7,10 +7,9 @@ import java.util.LinkedHashMap;
 public class CharacterCreator {
 
     public static void main(String[] args) {
-        Character character = new Character(false);
-        LinkedHashMap<Character.statName, Integer> stats = character.getStats();
-        for (Character.statName stat: stats.keySet()) {
-            System.out.printf("The value of %s is %s with mod %s\n", stat, stats.get(stat), Character.getStatMod(stats.get(stat)));
+        Character character = new Character(false, 1);
+        for(int i = 1; i < 21; i++) {
+            System.out.println((i / 4) + 1 + (i % 4 == 0 ? 0 : 1));
         }
     }
 }
